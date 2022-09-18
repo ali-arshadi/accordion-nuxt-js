ed74f8683cb35a5dedea76d4e1f8b6dd7041a5fb
Ali Arshadi <realgabral@protonmail.com>
2022-09-18 16:44:57 +0430

    adding scss to the nuxt js
    adding some default styles to the *

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'accordion',
+    title: 'accordion - Nuxt js',
     htmlAttrs: {
       lang: 'en',
     },
@@ -15,7 +15,7 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [],
+  css: ['@/assets/scss/global.scss'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
