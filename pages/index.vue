39626cda3ed66c57f32c98e64869105e31684935
Ali Arshadi <realgabral@protonmail.com>
2022-09-18 16:51:14 +0430

    adding the component to the page

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,17 @@
-<template></template>
+<template>
+  <div class="accordion-wrapper">
+    <Accordion />
+  </div>
+</template>
 
 <script lang="ts">
 import Vue from 'vue'
 
-export default Vue.extend({})
+export default Vue.extend({
+  components: {
+    Accordion: () => import('@/components/modules/accordion.vue'),
+  },
+})
 </script>
+
+<style lang="scss" scoped></style>
