38b6daad9e246963e3e534be337cb4025ebb722a
Ali Arshadi <realgabral@protonmail.com>
2022-10-30 16:32:12 +0330

    accordion created (can add some animations and svgs to it)

--- components/modules/accordion.vue
+++ components/modules/accordion.vue
@@ -1,10 +1,70 @@
 <template>
-  <div></div>
+  <div class="accordion-container-wrapper">
+    <div
+      class="accordion-wrapper"
+      v-for="(accordion, index) in accordionData"
+      :key="accordion.id"
+    >
+      <div class="accordion-title-wrapper" @click="toggleAccordion(index)">
+        <p>{{ accordion.title }}</p>
+      </div>
+      <div :class="['accordion-description-wrapper']">
+        <p>{{ accordion.description }}</p>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
-export default Vue.extend({})
+import Vue from 'vue';
+export default Vue.extend({
+  props: ['accordionData'],
+  data() {
+    return {
+      isOpen: false as boolean,
+    };
+  },
+  methods: {
+    toggleAccordion(index: number) {
+      const elementInfo = document.querySelectorAll(
+        '.accordion-description-wrapper'
+      )[index] as HTMLElement;
+
+      const elementStyle = getComputedStyle(elementInfo);
+
+      const elementDisplay = elementStyle.display;
+
+      if (elementDisplay == 'none') {
+        elementInfo.style.display = 'block';
+      } else {
+        elementInfo.style.display = 'none';
+      }
+    },
+  },
+});
 </script>
 
-<style lang="scss"></style>
+<style lang="scss" scoped>
+.accordion-container-wrapper {
+  width: 100%;
+}
+.accordion-wrapper {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+}
+.accordion-title-wrapper {
+  align-self: flex-start;
+  background-color: rgb(171, 162, 255);
+  width: 100%;
+  padding: 10px;
+}
+.accordion-description-wrapper {
+  align-self: flex-start;
+  width: 100%;
+  background-color: rgb(224, 240, 168);
+  padding: 10px;
+  display: none;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,51 @@
 <template>
-  <div class="accordion-wrapper">
-    <Accordion />
+  <div class="page-wrapper">
+    <div class="accordion-component-wrapper">
+      <Accordion :accordionData="accordion" />
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
+import Vue from 'vue';
 
 export default Vue.extend({
+  data() {
+    return {
+      accordion: [
+        {
+          id: '1234sdlkfj1',
+          title: 'this is test data',
+          description: 'this is test description',
+        },
+        {
+          id: 'alskdfjk14512k4lj',
+          title: 'this is test data',
+          description: 'this is test description',
+        },
+        {
+          id: '2103948akldfsj',
+          title: 'this is test data',
+          description: 'this is test description',
+        },
+      ],
+    };
+  },
   components: {
     Accordion: () => import('@/components/modules/accordion.vue'),
   },
-})
+});
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.page-wrapper {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+.accordion-component-wrapper {
+  width: 50%;
+  margin-top: 100px;
+}
+</style>
