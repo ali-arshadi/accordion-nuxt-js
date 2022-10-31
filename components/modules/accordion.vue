f24e2b71d68ff87d51494d13cab49dfa061d9868
Ali Arshadi <realgabral@protonmail.com>
2022-10-31 16:00:13 +0330

    animation added to the accordion (can add options)

--- components/modules/accordion.vue
+++ components/modules/accordion.vue
@@ -1,16 +1,13 @@
 <template>
   <div class="accordion-container-wrapper">
     <div
-      class="accordion-wrapper"
+      :class="['accordion-wrapper', active === index ? 'active' : '']"
       v-for="(accordion, index) in accordionData"
       :key="accordion.id"
+      @click="toggleAccordion"
     >
-      <div class="accordion-title-wrapper" @click="toggleAccordion(index)">
-        <p>{{ accordion.title }}</p>
-      </div>
-      <div :class="['accordion-description-wrapper']">
-        <p>{{ accordion.description }}</p>
-      </div>
+      <p class="accordion-title-wrapper">{{ accordion.title }}</p>
+      <p class="accordion-description-wrapper">{{ accordion.description }}</p>
     </div>
   </div>
 </template>
@@ -21,23 +18,41 @@ export default Vue.extend({
   props: ['accordionData'],
   data() {
     return {
+      active: null as any,
       isOpen: false as boolean,
     };
   },
   methods: {
-    toggleAccordion(index: number) {
-      const elementInfo = document.querySelectorAll(
+    toggleAccordion(e: any) {
+      //* variables
+      const parentElement = e.target.parentElement;
+
+      const descriptionElement = parentElement.querySelector(
         '.accordion-description-wrapper'
-      )[index] as HTMLElement;
+      );
 
-      const elementStyle = getComputedStyle(elementInfo);
+      const descriptionElementMaxHeight = descriptionElement.scrollHeight;
 
-      const elementDisplay = elementStyle.display;
+      const allItems = document.querySelectorAll('.accordion-wrapper');
 
-      if (elementDisplay == 'none') {
-        elementInfo.style.display = 'block';
+      //*check element have active class or not
+      if (parentElement.classList.contains('active')) {
+        //* remove active class and max height from item
+        parentElement.classList.remove('active');
+        descriptionElement.style.maxHeight = null;
       } else {
-        elementInfo.style.display = 'none';
+        //* remove active class and max height from all items
+        allItems.forEach((element: any) => {
+          element.classList.remove('active');
+          element.querySelector(
+            '.accordion-description-wrapper'
+          ).style.maxHeight = null;
+        });
+        //* add active class and max height for clicked item
+        parentElement.classList.add('active');
+        descriptionElement.style.maxHeight = `${
+          descriptionElementMaxHeight + 40
+        }px`;
       }
     },
   },
@@ -53,18 +68,49 @@ export default Vue.extend({
   align-items: center;
   justify-content: center;
   flex-direction: column;
-}
-.accordion-title-wrapper {
-  align-self: flex-start;
-  background-color: rgb(171, 162, 255);
-  width: 100%;
-  padding: 10px;
-}
-.accordion-description-wrapper {
-  align-self: flex-start;
-  width: 100%;
-  background-color: rgb(224, 240, 168);
-  padding: 10px;
-  display: none;
+  position: relative;
+  &:after {
+    content: '';
+    width: 10px;
+    height: 2px;
+    position: absolute;
+    top: 14px;
+    right: 14px;
+    background-color: black;
+  }
+  &:before {
+    content: '';
+    transition: all linear 0.2s;
+    width: 10px;
+    height: 2px;
+    position: absolute;
+    top: 14px;
+    right: 14px;
+    transform: rotate(90deg);
+    background-color: black;
+  }
+  .accordion-title-wrapper {
+    align-self: flex-start;
+    background-color: rgb(171, 162, 255);
+    width: 100%;
+    padding: 10px;
+    cursor: pointer;
+  }
+  .accordion-description-wrapper {
+    align-self: flex-start;
+    width: 100%;
+    background-color: rgb(224, 240, 168);
+    transition: 0.3s all linear;
+    max-height: 0;
+    overflow: hidden;
+  }
+  &.active {
+    .accordion-description-wrapper {
+      padding: 10px;
+    }
+    &:before {
+      transform: rotate(180deg);
+    }
+  }
 }
 </style>
